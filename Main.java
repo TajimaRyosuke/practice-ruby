55b3e4fad1f0826c73e336d664d3b0f9bd27563a
RyosukeTajima <east.helman56@gmail.com>
2021-08-11 09:29:42 +0000

    Java

--- Main.java
+++ Main.java
@@ -1,27 +1,27 @@
 class Main{
   public static void main(String [] args){
     System.out.println("Hello");
-    
+
     System.out.println(3 % 5);
-    
+
     String greeting = "Hello !!";
-    
-    String name = "Miyamoto"; 
+
+    String name = "Miyamoto";
     name = "Sasaki";
     System.out.println(greeting + " "  + name + " " + "Kojiro");
-    
+
     int number;
     number = 3;
     System.out.println(number = number + 1);
-    
+
     System.out.println(name == "Sasaki");
-    
+
     if(name == "Sasaki"){
       System.out.println("あなたの名前は" + name + "です" );
     } else {
       System.out.println("あなたは誰ですか？");
     }
-    
+
     String bigName = "Snake";
     // bigName = "camel";
     switch(bigName){
@@ -32,21 +32,25 @@ class Main{
         System.out.println("あなたに名前はありません");
         break;
     }
-    
+
     // int i = 10;
     // while(i > 0){
     //   System.out.println(i);
     //   i-= 1;
     // }
-    
+
     for(int i = 1; i <= 10; i+= 1){
       if(i % 3 == 0){
         continue;
       }
       System.out.println(i + "回ジャンプしました");
     }
-    
+
     String[] yourNames = {"yusuke", "junya", "emit"};
-    System.out.println("あなたの名前は" + yourNames[0] + "ですよ");
+    yourNames[0] = "seiya";
+
+    for(int i = 0; i < yourNames.length; i++){
+      System.out.println("あなたの名前は" + yourNames[i] + "ですよ");
+    }
   }
 }
