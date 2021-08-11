74cd509d5f7243918d2667d2ceab101dd1fd0514
RyosukeTajima <east.helman56@gmail.com>
2021-08-11 07:43:11 +0000

    Java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 class Main{
   public static void main(String [] args){
     System.out.println("Hello");
+    
     System.out.println(3 % 5);
     
+    String greeting = "Hello !!";
+    
     String name = "Sasaki"; 
-    System.out.println(name);
+    System.out.println(greeting + " "  + name + " " + "Kojiro");
     
     int number;
     number = 3;
