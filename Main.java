b076e378b6e57a96c6da9fad40a2271f575a9a91
RyosukeTajima <east.helman56@gmail.com>
2021-08-11 07:40:00 +0000

    Java

--- Main.java
+++ Main.java
@@ -2,5 +2,12 @@ class Main{
   public static void main(String [] args){
     System.out.println("Hello");
     System.out.println(3 % 5);
+    
+    String name = "Sasaki"; 
+    System.out.println(name);
+    
+    int number;
+    number = 3;
+    System.out.println(number);
   }
 }
