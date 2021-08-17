a08fe987cb7dd940f39df4efb5253525b94f7c06
RyosukeTajima <east.helman56@gmail.com>
2021-08-17 13:14:38 +0000

    updata

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ class Main{
         int num2 = 2 ;
         int num3 = num1 / num2 ;
         System.out.println(num3) ;
-        System.out.println("ここ数日うまくいってないけど")
+        System.out.println("ここ数日うまくいってないけど");
+        System.out.println("俺なら乗り越えられる");
     }
 }
