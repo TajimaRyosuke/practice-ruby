e533d80175dd75c0c062740d2e2506914cd3b9ca
RyosukeTajima <east.helman56@gmail.com>
2021-08-18 10:00:30 +0000

    updata

--- Main.java
+++ Main.java
@@ -1,13 +1,15 @@
-class Main{
-    public static void main(String[] args){
-      for(int i=0; i<5; i++){
-      for(int j=0; j<i; j++){
-        System.out.print(" ");
-      }
-      for(int k=0; k<(5-i)*2-1; k++){
-        System.out.print("*");
-      }
-      System.out.print("\n");
-    }
-    }
+import java.util.*;
+public class Main {
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		// 整数の入力
+		int a = scanner.nextInt();
+		// スペース区切りの整数の入力
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		// 文字列の入力
+		String s = scanner.next();
+		// 出力
+		System.out.println((a+b+c) + " " + s);
+	}
 }
