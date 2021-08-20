8c0051962afa37327a5f9b10bbf4b4c6462b296e
RyosukeTajima <east.helman56@gmail.com>
2021-08-20 13:48:19 +0000

    updata

--- Main.java
+++ Main.java
@@ -2,14 +2,9 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args){
 		Scanner scanner = new Scanner(System.in);
-		// 整数の入力
-		int a = scanner.nextInt();
-		// スペース区切りの整数の入力
-		int b = scanner.nextInt();
-		int c = scanner.nextInt();
-		// 文字列の入力
+			// 文字列の入力
 		String s = scanner.next();
 		// 出力
-		System.out.println((a+b+c) + " " + s);
+		System.out.println(s);
 	}
 }
