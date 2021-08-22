f183bc002d220b897a3213bdd7024f20646d41aa
RyosukeTajima <east.helman56@gmail.com>
2021-08-22 12:46:58 +0000

    updata

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
-import java.util.*;
-public class Main {
+class Main{
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-			// 文字列の入力
-		String s = scanner.next();
-		// 出力
-		System.out.println(s);
+		for(int i = 0; i < 5; i++){
+			for(int j=0; j < i; j++){
+				System.out.print(" ");
+			}
+			for(int k=0; k<(5-i)*2-1; k++){
+				System.out.print("*");
+			}
+			System.out.print("\n");
+
+		}
+
+		for(int l=0; l<5; l++){
+			for(int m=0; m<5-(l+1); m++){
+				System.out.print(" ");
+			}
+			for(int n = 0; n<(l+1)*2-1; n++){
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
 	}
 }
